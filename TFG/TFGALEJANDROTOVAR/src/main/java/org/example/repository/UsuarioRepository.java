091a502a236646fb9dca057210f3d02c344e75c1
Alejandrotovarlgns <alejandro.tovarbarroso24@campusfp.es>
2026-04-07 09:58:36 +0200

    Add files via upload

---
+++ TFG/TFGALEJANDROTOVAR/src/main/java/org/example/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsuarioAndPassword(String usuario, String password);
+}
---
+++ TFG/TFGALEJANDROTOVAR/src/main/java/org/example/service/UsuarioService.java
@@ -0,0 +1,19 @@
+package org.example.service;
+
+import org.example.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public String validarLogin(String user, String pass) {
+
+        return usuarioRepository.findByUsuarioAndPassword(user, pass)
+                .map(u -> u.getRol())
+                .orElse(null);
+    }
+}
